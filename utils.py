fe95261c0621cca9331aa8f6d2166312c9786d34
lmartinezcarbo <157326101+lmartinezcarbo@users.noreply.github.com>
2026-06-27 10:47:04 -0400

    feat: add utility function for displaying products

---
+++ utils.py
@@ -0,0 +1,6 @@
+def print_products(products):
+    print("\nID | Name | Qty | Price | Category")
+    print("-------------------------------------")
+
+    for p in products:
+        print(f"{p[0]} | {p[1]} | {p[2]} | {p[3]} | {p[4]}")
